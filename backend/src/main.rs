e467963108d34e84c5dce1035e2a9be29e54b64f
ram4ik <ram4ik@hotmail.com>
2024-02-09 15:41:12 +0200

    websocket example

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,14 @@
-#[rocket::get("/")]
-fn chat() {
+use rocket::futures::{SinkExt, StreamExt};
+use rocket_ws::{WebSocket, Channel};
 
+#[rocket::get("/")]
+fn chat(ws: WebSocket) -> Channel<'static> {
+    ws.channel(move |mut stream| Box::pin(async move {
+        while let Some(mesage) = stream.next().await {
+            let _ = stream.send(mesage?).await;
+        }
+        Ok(())
+    }))
 }
 
 #[rocket::main]
